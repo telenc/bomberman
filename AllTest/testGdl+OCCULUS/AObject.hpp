5d7c29e75c08086352f791734ba4e8a759722996
Remi Telenczak <remi.telenczak@epitech.eu>
2014-05-13 03:56:04 -0700

    ok

--- AllTest/testGdl+OCCULUS/AObject.hpp
+++ AllTest/testGdl+OCCULUS/AObject.hpp
@@ -1,79 +1,62 @@
 #include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp> 
-#include <iostream> 
+#include <glm/gtc/matrix_transform.hpp>
+#include <iostream>
 #include "GameEngine.hpp"
 #include <BasicShader.hh>
-// La classe abstraite representant un objet avec sa position, sa rotation et son echelle 
+// La classe abstraite representant un objet avec sa position, sa rotation et son echelle
 class AObject
 {
 public:
   AObject() :
-    _position(0, 0, 0), // On initialise la position a 0 
-    _rotation(0, 0, 0), // pareil pour la rotation 
+    _position(0, 0, 0), // On initialise la position a 0
+    _rotation(0, 0, 0), // pareil pour la rotation
     _scale(1, 1, 1) // l'echelle est mise a 1
   {
   }
   virtual ~AObject()
   {}
-  // La fonction initialize charge l'objet ou le construit 
-  virtual bool initialize() 
-  { 
+  virtual bool initialize()
+  {
     return (true);
   }
-  // La fonction update sert a gerer le comportement de l'objet
   virtual void update(gdl::Clock const &clock, gdl::Input &input)
   {}
-  // La fonction draw sert a dessiner l'objet 
   virtual void draw(gdl::AShader &shader, gdl::Clock const &clock) = 0;
-  void translate(glm::vec3 const &v) 
-  { 
-
-    _position += v; 
-
-  }
-  void rotate(glm::vec3 const& axis, float angle) 
+  void translate(glm::vec3 const &v)
   {
-    _rotation += axis * angle; 
+    _position += v;
   }
-  void scale(glm::vec3 const& scale) 
+  void rotate(glm::vec3 const& axis, float angle)
   {
-    _scale *= scale; 
+    _rotation += axis * angle;
   }
-  glm::mat4 getTransformation() 
+  void scale(glm::vec3 const& scale)
   {
-    glm::mat4 transform(1); // On cree une matrice identite 
-
- 
-
-    // On applique ensuite les rotations selon les axes x, y et z 
+    _scale *= scale;
+  }
+  glm::mat4 getTransformation()
+  {
+    glm::mat4 transform(1);
 
     transform = glm::rotate(transform, _rotation.x, glm::vec3(1, 0, 0));
-    transform = glm::rotate(transform, _rotation.y, glm::vec3(0, 1, 0)); 
+    transform = glm::rotate(transform, _rotation.y, glm::vec3(0, 1, 0));
 
-    transform = glm::rotate(transform, _rotation.z, glm::vec3(0, 0, 1)); 
+    transform = glm::rotate(transform, _rotation.z, glm::vec3(0, 0, 1));
+    transform = glm::translate(transform, _position);
+    transform = glm::scale(transform, _scale);
+    return (transform);
+  }
 
-    // On effectue ensuite la translation 
 
-    transform = glm::translate(transform, _position); 
 
-    // Et pour finir, on fait la mise a l'echelle 
+protected:
 
-    transform = glm::scale(transform, _scale); 
+  glm::vec3 _position;
 
-    return (transform); 
+  glm::vec3 _rotation;
 
-  } 
+  glm::vec3 _scale;
 
- 
 
-protected: 
-
-  glm::vec3 _position; 
-
-  glm::vec3 _rotation; 
-
-  glm::vec3 _scale; 
-
- 
 
 };
