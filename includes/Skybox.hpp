2f1bb02f01c1d3e2fc27bea64a8e299b5d617cb9
Remi telenczak <remi.telenczak@epitech.eu>
2014-05-22 09:01:21 -0700

    ok

--- includes/Skybox.hpp
+++ includes/Skybox.hpp
@@ -5,7 +5,7 @@
 // Login   <remi@epitech.net>
 //
 // Started on  Thu May 22 07:22:23 2014 Remi telenczak
-// Last update Thu May 22 07:27:06 2014 Remi telenczak
+// Last update Thu May 22 09:00:37 2014 Remi telenczak
 //
 
 #ifndef		_SKYBOX_BOMBER
@@ -17,7 +17,10 @@ class Skybox : public AObjectPhysic
 {
 public:
   Skybox(Map *, ModelList *, EventManager *);
+  //void eventPlayerMove(void *);
   ~Skybox();
   virtual void update(gdl::Clock const &clock, gdl::Input &input);
+private:
+  //ICallBack *callPlayerMove;
 };
 #endif
