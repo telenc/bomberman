709257d2415037a517e1194d57acd102687da7d1
Remi telenczak <remi.telenczak@epitech.eu>
2014-06-03 07:25:19 -0700

    ok

---
+++ includes/MenuNumber.hpp
@@ -0,0 +1,48 @@
+//
+// MenuNumber.hpp for mEnu in /home/remi/cpp_bomberman
+//
+// Made by Remi telenczak
+// Login   <remi@epitech.net>
+//
+// Started on  Mon Jun  2 02:10:08 2014 Remi telenczak
+// Last update Tue Jun  3 07:11:08 2014 Remi telenczak
+//
+
+#ifndef		_MENUNUMBER
+# define	_MENUNUMBER
+
+# include	"ModelList.hpp"
+# include	"EventManager.hpp"
+# include	<Clock.hh>
+# include	<BasicShader.hh>
+# include	<Input.hh>
+
+class MenuBoxNumber;
+class MenuNumber
+{
+public:
+  MenuNumber(ModelList *, EventManager *, gdl::Clock *);
+  ~MenuNumber();
+  void	draw(gdl::BasicShader &, gdl::Clock const &);
+  int	update(gdl::Clock &, gdl::Input &);
+  void	eventKeyUp(void *data);
+void	eventKeyA(void *data);
+  void	eventKeyDown(void *data);
+void	upIt(std::list<int>::iterator );
+void	downIt(std::list<int>::iterator);
+int	getResult();
+private:
+  ModelList	*_mod;
+  EventManager	*_event;
+  gdl::Clock *_clock;
+  std::list<MenuBoxNumber *>	listBox;
+  std::list<MenuBoxNumber *>	listBoxToDelete;
+  ICallBack	*callKeyUp;
+  ICallBack	*callKeyDown;
+  ICallBack	*callKeyA;
+std::list<int>		result;
+int		_nextX;
+std::map<int, float> rotationCube;
+};
+
+#endif
---
+++ src/Bloc/MenuBoxNumber.cpp
@@ -0,0 +1,86 @@
+//
+// defaultWall.cpp for dede in /home/remi/cpp_bomberman
+//
+// Made by Remi telenczak
+// Login   <remi@epitech.net>
+//
+// Started on  Thu May 15 09:26:53 2014 Remi telenczak
+// Last update Tue Jun  3 04:10:11 2014 Remi telenczak
+//
+
+#include	"MenuBoxNumber.hpp"
+#include	"ModelList.hpp"
+
+MenuBoxNumber::MenuBoxNumber(Map *map, ModelList *model, EventManager *event, gdl::Clock *clock) : ABloc(map, model, event, clock)
+{
+  this->_skin = model->getModel("menu1");
+  this->_height = 3;
+  this->_died = false;
+  this->_width = 3;
+  this->_ytarget = 0;
+  this->_position.z = -25;
+  this->menu1 = model->getModel("menu1");
+  this->menu2 = model->getModel("menu2");
+  this->menu3 = model->getModel("menu3");
+  this->menu4 = model->getModel("menu4");
+}
+
+void	MenuBoxNumber::goDelete()
+{
+  this->_ytarget = -20;
+}
+
+bool MenuBoxNumber::update(gdl::Clock const &clock, gdl::Input &input)
+{
+  if (this->_died == true || this->get_y() <=  -18)
+    {
+      return false;
+    }
+  //std::cout << this->get_y() << std::endl;;
+  if (this->_ytarget != this->get_y())
+    {
+      this->translate(glm::vec3(0, (this->_ytarget - this->get_y()) / 10, 0));
+    }
+  else
+    {
+      this->set_y(0);
+    }
+  if (this->_targetRot < this->get_rotx())
+    {
+      this->rotate(glm::vec3(1, 0, 0), -1);
+    }
+  else if (this->_targetRot > this->get_rotx())
+    {
+      this->rotate(glm::vec3(1, 0, 0), 1);
+    }
+    (void)clock;
+  (void)input;
+  //this->_skin->setCurrentAnim(22);
+  (void)clock;
+  (void)input;
+  return true;
+}
+
+void	MenuBoxNumber::setTargetRotation(float rot)
+{
+  if (rot >= 0 && rot <= 320)
+    this->_skin = menu1;
+  else if (rot > 320 && rot <= 585)
+    {
+      rot += 90;
+      this->_skin = menu2;
+    }
+  else if (rot >585 && rot <= 855)
+    {
+      rot+= 180;
+      this->_skin = menu3;
+    }
+  else
+    this->_skin = menu4;
+  this->_targetRot = rot;
+}
+
+bool MenuBoxNumber::fireTouch()
+{
+  return true;
+}
