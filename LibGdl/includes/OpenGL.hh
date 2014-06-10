c2bb0030f6c5e3e4ee0a1a1f39ef25ef2b186a6f
Remi Telenczak <remi.telenczak@onprenduncafe.com>
2014-06-10 14:47:54 +0200

    aplha

--- LibGdl/includes/OpenGL.hh
+++ LibGdl/includes/OpenGL.hh
@@ -7,7 +7,12 @@
 # include	<GL\GL.h>
 # include	<GL\GLU.h>
 # include	<GL\glext.h>
+#else 
+#ifdef __APPLE__
+# include       <OpenGl/gl.h>
+# include       <OpenGl/glu.h>
 #else
 # include	<GL/gl.h>
 # include	<GL/glu.h>
+#endif
 #endif
