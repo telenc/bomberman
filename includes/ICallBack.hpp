80f399d9f80e30b19e0178db0fcfcd95bd8dc599
martre_s <martre_s@Elitebook-martre.(none)>
2014-06-15 19:28:48 +0200

    ok

--- includes/ICallBack.hpp
+++ includes/ICallBack.hpp
@@ -5,7 +5,7 @@
 // Login   <remi@epitech.net>
 //
 // Started on  Wed May  7 07:57:15 2014 Remi telenczak
-// Last update Tue Jun 10 12:41:41 2014 Remi telenczak
+// Last update Sun Jun 15 19:15:27 2014 Steven Martreux
 //
 
 #ifndef		_ICALLBACK
@@ -18,4 +18,4 @@ public:
   virtual ~ICallBack() {};
 };
 
-#endif
+#endif		/* _ICALLBACK */
