cb0a5dbd1a80e53323e1d442fb310c8e702fde1c
martre_s <martre_s@Elitebook-martre.(none)>
2014-06-15 18:43:08 +0200

    ok

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,7 @@
 // Login   <remi@epitech.net>
 //
 // Started on  Tue May 13 04:21:54 2014 Remi telenczak
-<<<<<<< HEAD
-<<<<<<< HEAD
-// Last update Sun Jun 15 16:53:57 2014 Remi telenczak
-=======
-// Last update Sun Jun 15 07:10:28 2014 Steven Martreux
->>>>>>> 2c4c4cadbef375c9030ec89010ba100e149344a4
-=======
-// Last update Sun Jun 15 12:06:33 2014 Steven Martreux
->>>>>>> 2c520a45a5444d70184b94c02915bf43f88178dd
+// Last update Sun Jun 15 18:37:25 2014 Steven Martreux
 */
 
 #include <iostream>
@@ -47,7 +39,7 @@ int	main(int ac, char **av)
 
 	  //save = new LoadGame("save/test.xml", load->getEventManager(), load->getModel(), engine->getClock());
 	  //m = save->getMap();
-	  GenereMap gen(41, 41, 0, load->getEventManager(), load->getModel(), engine->getClock(), ZOMBIE);
+	  GenereMap gen(41, 41, 10, load->getEventManager(), load->getModel(), engine->getClock());
 	  Map *m = gen.getMap();
 	  Controller	joystick(load->getEventManager());;
 	  m->setSkybox(new Skybox(m, load->getModel(), load->getEventManager(), engine->getClock()));
