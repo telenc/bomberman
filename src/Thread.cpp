f8953ed74d657369c27f01312390459e3607c096
martre_s <martre_s@Elitebook-martre.(none)>
2014-06-15 20:07:29 +0200

    ok

---
+++ src/Thread.cpp
@@ -0,0 +1,23 @@
+//
+// Thread.cpp for Thread in /home/martre_s/cpp_bomberman
+// 
+// Made by Steven Martreux
+// Login   <martre_s@epitech.net>
+// 
+// Started on  Tue May  6 15:00:47 2014 Steven Martreux
+// Last update Sun Jun 15 20:06:11 2014 Steven Martreux
+//
+
+#include	"Thread.hpp"
+
+Thread::Thread(void *(*fct)(void *), void *data, const std::string & type)
+{
+  if (pthread_create(&(this->_thread_ing), NULL, fct, data) != 0)
+    throw new myException("Error create thread " + type);
+}
+
+Thread::~Thread()
+{
+  if (pthread_cancel(this->_thread_ing) != 0)
+    throw new myException("Error close thread");
+}
